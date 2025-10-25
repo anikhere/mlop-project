dd44d95e218f53873588b981679a74ab859ce300
anikhere <tahaanik729@gmail.com>
2025-10-25 12:04:00 +0530

    lasso model

--- src/train.py
+++ src/train.py
@@ -1,12 +1,12 @@
 import pickle
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
 
 data = load_diabetes()
 x_train, x_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
 x,y = data.data, data.target
-model = LinearRegression()
+model = Lasso()
 model.fit(x_train, y_train)
 model.predict(x_test)
 with open('model.pkl','wb') as f:
