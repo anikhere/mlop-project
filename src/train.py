771c352bd0421014db83d6a9d6968e36637db169
anikhere <tahaanik729@gmail.com>
2025-10-25 11:50:17 +0530

    this is the new ml file

--- src/train.py
+++ src/train.py
@@ -1,11 +1,14 @@
 import pickle
 from sklearn.linear_model import LinearRegression
 from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
 
 data = load_diabetes()
+x_train, x_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
 x,y = data.data, data.target
 model = LinearRegression()
-model.fit(x, y)
+model.fit(x_train, y_train)
+model.predict(x_test)
 with open('model.pkl','wb') as f:
     pickle.dump(model,f)
 print(f'the model dump is done')
